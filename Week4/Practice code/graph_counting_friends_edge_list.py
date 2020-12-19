fcb009ae5e9d27834c48bf5c105037af8349b8d0
gauravkkaushik <gaurav.k.kaushik@gmail.com>
2020-12-18 18:49:43 -0600

    Week4 (#3)
    * Material arrangement
    
    * week 3 study material
    
    * week 3 study material
    
    * Week 4 study
    
    * additional material and rearrangement
    
    * Week 4 sutdy material and assignment

---
+++ Week4/Practice code/graph_counting_friends_edge_list.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+#pairs of non directional graph pair set
+data1 = {'A':[1,1,1,2,2,3,3,3,3],
+'B':[2,3,4,3,5,4,5,6,7]
+        }
+
+ABdf = pd.DataFrame.from_dict(data1)
+
+data2 = {'B':[1,1,1,2,2,3,3,3,3],
+'C':[2,3,4,3,5,4,5,6,7]
+        }
+
+BCdf = pd.DataFrame.from_dict(data2)
+
+#join both DFs
+
+joinDF = pd.merge(ABdf,BCdf,how='inner',on = 'B')
+
+x = joinDF.groupby(['A','C']).count().reset_index()
+print(x[x.A ==1])
